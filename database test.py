8613c56ea141ba8146fec6d724b48d6d41bad47a
Archit-Jain05 <architjain2005@gmail.com>
2023-05-13 15:55:37 +0530

    12

--- database test.py
+++ database test.py
@@ -7,11 +7,40 @@ conn = mysql.connector.connect(
   database="vianewscredentials"
 )
 if conn:
-  print("Connection Established Successfully")
+  #print("Connection Established Successfully")
+  pass
 else:
   print("Connection Error")
 
 cur=conn.cursor()
-cur.execute("SELECT * FROM users")
-for row in cur:
-    print(row)
+
+def getall():
+    cur.execute("SELECT * FROM users")
+    for row in cur:
+        print(row)
+        
+
+def printres(cur):
+   for row in cur:
+      print(row)
+
+def validate(usn):
+    flag=0
+    sql = "SELECT * FROM users WHERE username LIKE \""+usn+"\""
+    cur.execute(sql)
+    for row in cur:
+       flag=1
+    if flag==1:
+       print("Valid")
+    else:
+       print("Invalid")
+
+#validate("Vanshita")
+
+def newuser(username,password,age,country,opt1,opt2,opt3,opt4,opt5,opt6,op7):
+   age=str(age)
+   opt1
+   sql="insert into users VALUES(\""+username+"\",\""+password+"\","+age+",\""+country+"\")"
+   print(sql)
+
+newuser("Iqra","123",19,"India")
